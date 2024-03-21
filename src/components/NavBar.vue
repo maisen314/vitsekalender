fe558102e007fa64f43e0a2a38bcce8856998a23
Kristoffer Høisæther <kristoffer.hoisether@sbanken.no>
2024-03-21 08:29:36 +0100

    wip

---
+++ src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <div class="header">
+      <RouterLink to="/" class="nonLink">Vitsekalender</RouterLink>
+      <nav class="navbar">
+        <RouterLink to="/" class="navLink">Home</RouterLink>
+        <RouterLink to="/sporsmal" class="navLink">Home2</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style>
+.header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: black;
+  padding: 20px 40px;
+  font-family: monospace;
+}
+.navbar {
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+}
+.navLink {
+  color: white;
+  font-size: large;
+  &:hover {
+    font-weight: 900;
+  }
+}
+.nonLink {
+  text-decoration: none;
+  color: white;
+  font-size: x-large;
+}
+</style>
---
+++ src/components/SubmitButton.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  click: Function
+})
+</script>
+
+<template>
+  <!-- pass onClick from parent -->
+  <button type="submit" class="button" :onclick="click">
+    <slot>Send inn</slot>
+  </button>
+</template>
+
+<style>
+.button {
+  background-color: rgb(14, 79, 79);
+  color: white;
+  border-radius: 4px;
+  padding: 4px;
+  &:hover {
+    color: black;
+    background-color: rgb(155, 255, 255);
+  }
+}
+</style>
